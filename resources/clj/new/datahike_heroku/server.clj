aa9deba0096344ee64edc01ccfcc5659c6030686
Dieter Komendera <dieter@komendera.com>
2020-12-23 23:17:22 +0100

    Functional web app with db

--- resources/clj/new/datahike_heroku/server.clj
+++ resources/clj/new/datahike_heroku/server.clj
@@ -1,25 +1,65 @@
 (ns {{namespace}}.server
     (:require [clojure.string :as str]
-              [ring.adapter.jetty :as jetty]))
+              [datahike-postgres.core]
+              [datahike.api :as d]
+              [ring.adapter.jetty :as jetty]
+              [ring.middleware.params :as params]))
+
+(def app-state (atom {:conn nil
+                      :server nil}))
 
 (defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
+  (when-let [todo (get-in request [:params "todo"])]
+    (d/transact (:conn @app-state) [{:todo/name todo}]))
+
+  (let [todos (d/q '[:find [?name ...]
+                     :where [_ :todo/name ?name]]
+                   (d/db (:conn @app-state)))]
+    {:status  200
+     :headers {"Content-Type" "text/html"}
+     :body    (str "Todo items: <br/>" (str/join "<br/>" todos))}))
+
+(def app
+  (params/wrap-params handler))
+
+(def schema
+  [{:db/ident :todo/name
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}])
 
 (defn env-port []
   (some-> (System/getenv "PORT") Integer/parseInt))
 
 (defn env-db-config []
-  (let [uri                 (java.net.URI. (System/getenv "DATABASE_URL"))
-        [username password] (str/split (.getUserInfo uri) #":")]
-    {:backend  :pg
-     :host     (.getHost uri)
-     :username username
-     :password password
-     :path     (.getPath uri)
-     :port     (.getPort uri)}))
+  (when-let [db-url (System/getenv "DATABASE_URL")]
+    (let [uri                 (java.net.URI. db-url)
+          [username password] (str/split (.getUserInfo uri) #":")]
+      {:store
+       {:backend  :pg
+        :host     (.getHost uri)
+        :username username
+        :password password
+        :path     (.getPath uri)
+        :port     (.getPort uri)}})))
+
+(defn start-db! []
+  (let [db-config (or (env-db-config)
+                      {:store {:backend :mem :id "server"}})]
+    (when-not (d/database-exists? db-config)
+      (d/create-database db-config))
+
+    (let [conn (d/connect db-config)]
+      (d/transact conn schema)
+      (swap! app-state assoc :conn conn))))
+
+(defn start-http! []
+  (let [port   (or (env-port) 3000)
+        server (jetty/run-jetty #'app {:port port :join? false})]
+    (swap! app-state assoc :server server)))
 
 (defn -main []
-  (let [port (or (env-port) 3000)]
-    (jetty/run-jetty handler {:port port})))
+  (start-db!)
+  (start-http!))
+
+(comment
+  (swap! app-state update :server #(.stop %)))
